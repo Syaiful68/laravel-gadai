aba90f1c4a2aaf6e5ed8c4d5a8fa68bba17eb055
Syaiful68 <syaifulSheva@gmail.com>
2025-10-24 09:25:44 +0700

    perbaikan bug

--- resources/js/components/paginationView.vue
+++ resources/js/components/paginationView.vue
@@ -7,10 +7,10 @@ const props = defineProps({
 <template>
     <div class="col-auto">
         <ul class="pagination m-0 ms-auto">
-            <li class="page-item">
+            <li class="page-item" v-if="props.data.prev_page_url !== null">
                 <a class="page-link" :href="props.data.prev_page_url">< Prev</a>
             </li>
-            <li class="page-item">
+            <li class="page-item" v-if="props.data.next_page_url !== null">
                 <a class="page-link" :href="props.data.next_page_url">Next ></a>
             </li>
         </ul>
